6e216187d35985aee59cb70f86611d261e12b6da
Justin Horein <justinhorein@gmail.com>
2021-03-04 02:59:00 -0800

    need permutations function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as math from 'mathjs';
+import { permutations } from 'mathjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      display: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  permutations(arr) {
+    
+}
+
+  compPerms(n) {
+    let arr = n.split(',')
+    // add all permutations to array
+    let perms = permutations(arr);
+    // update state
+    this.setState({display: perms})
+  }
+
+  handleChange(event) {
+    this.setState({data: event.target.value});
+    // console.log(this.state.data)
+  }
+
+
+  handleSubmit(event) {
+    alert('Data was submitted: ' + this.state.data);
+    event.preventDefault();
+    // this.setState({data:})
+    this.compPerms(this.state.data);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Data Set:
+            <input type="text" value={this.state.data} onChange={this.handleChange} placeholder={[1,2,3]} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <h3>Permutations</h3>
+        <div>{ this.state.display }</div>
+      </div>
+    );
+  }
 }
 
 export default App;
