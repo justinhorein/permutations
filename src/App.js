bab1e0113e9020c0f903bd805a4ab8e938e543e4
Justin Horein <justinhorein@gmail.com>
2021-03-16 03:11:51 -0700

    need to add totPerms to state, need to display them

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
 import React from 'react';
 import './App.css';
 import * as math from 'mathjs';
-import { permutations } from 'mathjs';
+import { cos, permutations } from 'mathjs';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-      display: 0
+      data: [1,2,3],
+      display: 6
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  permutations(arr) {
-    
+  permutations(arr) {  
+    var permArr = [], usedChars = [];
+    function permute(input) {          
+          var i, ch;
+          for (i = 0; i < input.length; i++) {
+            ch = input.splice(i, 1)[0];
+            usedChars.push(ch);
+            if (input.length === 0) {
+              permArr.push(usedChars.slice());
+            }
+            permute(input);
+            input.splice(i, 0, ch);
+          
+            usedChars.pop();
+          }
+          // return permArr
+    } 
+    permute(arr);
+    // display permutations
+    let list = document.querySelector("#perms");
+    permArr.forEach((i) => {
+      let span = document.createElement('span');
+      list.appendChild(span);
+      span.innerHTML += i;
+    })
 }
 
   compPerms(n) {
-    let arr = n.split(',')
-    // add all permutations to array
-    let perms = permutations(arr);
+    // number of items in array
+    let count = n.length;
+    // number of possible permutations
+    let num = math.permutations(count);
+    // Get list of all permutations
+    this.permutations(n);
+    // console.log(perms);
     // update state
-    this.setState({display: perms})
+    this.setState({display: num})
+    // console.log(this.state.totalPerms);
   }
 
   handleChange(event) {
-    this.setState({data: event.target.value});
+    this.setState({data: event.target.value.split(',')});
     // console.log(this.state.data)
   }
 
@@ -51,8 +79,11 @@ class App extends React.Component {
           </label>
           <input type="submit" value="Submit" />
         </form>
-        <h3>Permutations</h3>
-        <div>{ this.state.display }</div>
+        <div class="display">
+          <h3>Permutations</h3>
+          <div class="total">total: { this.state.display }</div>
+          <div id="perms"></div>
+        </div>
       </div>
     );
   }
